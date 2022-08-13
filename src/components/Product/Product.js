da7a7f0c94e6bfb0d3a684598a0458092dc74bba
Rabiul hossain <mohammadrabiul4@gmail.com>
2022-08-13 11:18:07 -0700

    event handler added.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
-    const { name, img, seller, price } = props.product;
+    const {handleAddToCart, product} = props;
+    const { name, img, seller, price, ratings } = product;
     return (
         <div className='product'>
             <img src={img} alt=""/>
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: {price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings:{ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={ faShoppingCart }/>
+            </button>
         </div>
     );
 };
